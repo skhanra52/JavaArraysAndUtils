338c517f67a165e9957f98bc7df1401e83500fe3
Suman Kalyan Khanra <skhanra52@gmail.com>
2025-04-18 18:02:14 +0530

    Project description

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This project contains all the java common" +
+                " array and Array utils related example ");
     }
 }
