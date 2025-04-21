36c163a8c73cdb0563e5f3bdf416148539a7f70e
Suman Kalyan Khanra <skhanra52@gmail.com>
2025-04-21 10:31:36 +0530

    Few of the list examples has been added

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,25 @@
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("This project contains all the java common" +
                 " array and Array utils related example ");
 
-        int [] sampleInt = new int[5];
-        for(int i=0; i<sampleInt.length; i++){
-            sampleInt[i] = sampleInt.length - i;
-        }
+//        int [] sampleInt = new int[5];
+//        for(int i=0; i<sampleInt.length; i++){
+//            sampleInt[i] = sampleInt.length - i;
+//        }
         // Reading the array element using enhanced for loop.
-        for(int element: sampleInt){
-            System.out.println("Elements in array: " + element);
-        }
-        System.out.println("---------------------------------");
+//        for(int element: sampleInt){
+//            System.out.println("Elements in array: " + element);
+//        }
+//        System.out.println("---------------------------------");
         // Random number create and return function assigned to newly created array.
-        int [] unsortedArr = getRandomNumbers(10);
-        System.out.println(Arrays.toString(unsortedArr));
-        int [] copyOfUnsortedArr = Arrays.copyOf(unsortedArr, unsortedArr.length);
-        int [] reverseSort = descendingSort(copyOfUnsortedArr);
-        System.out.println(Arrays.toString(reverseSort));
+//        int [] unsortedArr = getRandomNumbers(10);
+//        System.out.println(Arrays.toString(unsortedArr));
+//        int [] copyOfUnsortedArr = Arrays.copyOf(unsortedArr, unsortedArr.length);
+//        int [] reverseSort = descendingSort(copyOfUnsortedArr);
+//        System.out.println(Arrays.toString(reverseSort));
 
         // Sorting it in ascending order.
 //        Arrays.sort(copyOfUnsortedArr);
@@ -47,10 +45,111 @@ public class Main {
 //       int minElement = findMin(userEntries);
 //       System.out.println("Minimum element of the array is: "+minElement);
 
-        int[] nums = {1,2,3,4,5,6};
-        reverse(nums);
-        System.out.println(Arrays.toString(nums));
-        reverseThroughMid(nums);
+//        int[] nums = {1,2,3,4,5,6};
+//        reverse(nums);
+//        System.out.println(Arrays.toString(nums));
+//        reverseThroughMid(nums);
+
+        //------------- Example of ArrayList<> has been added below.----------------------------------
+
+//        Key Methods in List Interface:
+//        add(E e)
+//        add(int index, E element)
+//        get(int index)
+//        set(int index, E element)
+//        remove(int index)
+//        remove(Object o)
+//        contains(Object o)
+//        indexOf(Object o)
+//        size()
+//        isEmpty()
+//        clear()
+
+        // ArrayList is a concrete class in the Java Collections Framework.
+        // It implements the List interface, along with a few others like
+        // RandomAccess, Cloneable, and Serializable.
+
+        ArrayList <GroceryItems> groceryItems = new ArrayList<>();
+        // Simply adding the elements to the end of the groceryItems List.
+        groceryItems.add(new GroceryItems("Milk"));
+        groceryItems.add(new GroceryItems("Butter"));
+        groceryItems.add(new GroceryItems("Apple", "Fruits", 2));
+
+        // If we want to add items at the beginning of the groceryItems,
+        // We can override the add with index value and item.
+        // Zero is the index value where I should add the item
+        groceryItems.add(0, new GroceryItems("Oranges", "Fruits", 6));
+        groceryItems.add(3, new GroceryItems("Banana", "Fruits", 9));
+
+        // To replace an entry we can use set method which will take the index value which should be replaced
+        // and the item value which will be newly placed.
+        // Butter will be replaced by Curd.
+        groceryItems.set(2, new GroceryItems("Curd"));
+
+        // To remove the item from the groceryItems we can use remove method which accept the index value to remove.
+        // Item Curd will be removed from the new ArrayList.
+        groceryItems.remove(2);
+
+        // Traversing the ArrayList created with GroceryItems object.
+        for(GroceryItems item: groceryItems){
+            System.out.println("Here is item: "+item);
+        }
+        // Display the entire groceryItems once.
+        System.out.println(groceryItems);
+
+        // Example of adding one list to another.
+        String[] items = {"Apple","Orange","Banana"};
+        List<String> list = List.of(items);
+        ArrayList<String> fruits = new ArrayList<>(list);
+        System.out.println(fruits);
+        String[] moreItems = {"Grapes", "Pineapple","Jack fruit"};
+        List<String> moreItemList = List.of(moreItems);
+        // We are adding two list here (adding moreItemList to fruits).
+        fruits.addAll(moreItemList);
+        System.out.println("Final fruits list: " + fruits);
+
+        // Example of retrieving the data from the list. For which we use get() method.
+        String itemAtThird = fruits.get(3);
+        System.out.println("Here is the item at third position: "+itemAtThird);
+        int itemFound = findItemInList(fruits, "banana");
+        System.out.println("Item found at the position: "+itemFound);
+
+        // Remove all the items from the List which are found in the original List.
+        fruits.removeAll(List.of("Grapes","Jack fruit"));
+        System.out.println("After remove All: "+fruits);
+
+        // retainAll() returns the only those item you would like to keep in the list
+        fruits.retainAll(List.of("Apple", "Orange", "Banana", "Grapes"));
+        System.out.println("After retention:  "+fruits);
+
+        // Emptying the List and checking if the List is empty.
+        fruits.clear();
+        System.out.println("After clearing the List: "+fruits);
+        System.out.println("Check if the list is empty " + fruits.isEmpty());
+
+        // Adding all the values back to List.
+        fruits.addAll(Arrays.asList("Apple","Orange","Banana","Grapes","Pineapple","Jack fruit"));
+        System.out.println("After adding items in fruits list: "+fruits);
+
+        // Sorting the List.
+        fruits.sort(Comparator.naturalOrder());
+        System.out.println("After sorting the List in alphabetical order: "+fruits);
+        // Sorting the List in reverse order.
+        fruits.sort(Comparator.reverseOrder());
+        System.out.println("After sorting in reverse Order: "+fruits);
+        GroceryItems.chooseMenu();
+    }
+
+    public static int findItemInList(ArrayList<String> fruits, String itemToFind){
+        int index = -1;
+        for(String item: fruits){
+            String targetItem = item.toLowerCase().trim();
+            if(targetItem.equals(itemToFind.toLowerCase().trim())){
+                index = fruits.indexOf(item);
+                break;
+            }
+        }
+        return index;
     }
 
     public static int[] getIntegers(int size){
@@ -171,4 +270,80 @@ public class Main {
         }
     }
 
+}
+
+// Record created to use in the example of ArrayList<>.
+record GroceryItems(String name, String type, int count){
+    public GroceryItems(String name){
+        this(name,"Dairy", 1);
+    }
+public static void chooseMenu(){
+        System.out.println("""
+                Available actions:\s
+                0 - to shutdown;
+                1 - to add item(s) to list (comma-delimited list).
+                2 - to remove any item (comma-delimited list).
+                """);
+        System.out.println("Enter a number for which action you want to do: ");
+        Scanner scanner = new Scanner(System.in);
+        int selectedOption = scanner.nextInt();
+        ArrayList<GroceryItems> groceryItems = new ArrayList<>();
+        switch (selectedOption){
+            case 0 :
+                break;
+            case 1:
+                groceryItems = addItemToList();
+            case 2:
+                // To remove any item (comma-delimited list).
+                removeItem(groceryItems);
+                break;
+            default:
+                // Do something
+                for (GroceryItems groceryItem : groceryItems) {
+                    System.out.println("Default grocery list" + groceryItems);
+                }
+
+        }
+}
+
+    public static ArrayList<GroceryItems> addItemToList (){
+        System.out.println("To add item(s) to grocery please enter comma-delimited values");
+        Scanner scanner = new Scanner(System.in);
+        String enteredValue = scanner.nextLine().trim();
+        String[] groceryItemTobeAdded = enteredValue.split(",");
+        ArrayList<GroceryItems> groceryItems = new ArrayList<>();
+        ArrayList<String> noDuplicate = new ArrayList<>();
+        for(String grocery: groceryItemTobeAdded){
+            if(noDuplicate.isEmpty() || !noDuplicate.contains(grocery)){
+                noDuplicate.add(grocery);
+            }
+        }
+        for(String grocery : noDuplicate){
+            groceryItems.add( new GroceryItems(grocery));
+        }
+        // Printing the entered items
+        System.out.println("Grocery Items: "+groceryItems);
+        return groceryItems;
+    }
+
+    public static ArrayList<GroceryItems> removeItem(ArrayList<GroceryItems> availableItems){
+        System.out.println("Please enter value or comma-delimited values to remove");
+        Scanner scanner = new Scanner(System.in);
+        String enteredValue = scanner.nextLine().trim();
+        String[] itemToRemove = enteredValue.split(",");
+        ArrayList<GroceryItems> groceryItems = new ArrayList<>();
+        System.out.println("Here is/are the item(s) user wants to remove:  " +Arrays.toString(itemToRemove));
+        for (var item: availableItems){
+            for(var removeItem: itemToRemove ){
+                if(item.name.trim().equals(removeItem.trim())){
+                    groceryItems.add(item);
+                }
+            }
+        }
+        availableItems.removeAll(groceryItems);
+//       sorting the GroceryItems by name
+        availableItems.sort(Comparator.comparing(GroceryItems::name));
+        System.out.println("After removing item(s)"+availableItems);
+        return availableItems;
+    }
 }
